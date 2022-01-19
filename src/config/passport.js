41a7f3f3b731d9e5427a6fa00b75be3f762d4ef9
Craig <indigomx9@gmail.com>
2022-01-18 20:36:12 -0500

    Adding Passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -25,6 +25,17 @@ passport.use(
     })
 );
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    })
+});
+
+
 
 
 
